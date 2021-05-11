594fc0aa87d2bbd2390ec26edf78be15251aefc3
ekinmemis <ekinmemis@outlook.com>
2021-05-12 00:35:37 +0300

    Add project files.

---
+++ Presentation/Appcent.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Appcent.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
